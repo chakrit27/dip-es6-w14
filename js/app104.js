76d83aae669e0be5b2eb1269ebe70cc6cc217442
chakrit27 <chakrit.indy2001@gmail.com>
2021-09-20 10:50:31 +0700

    update

---
+++ js/app104.js
@@ -0,0 +1,17 @@
+let firstname = 'Michel';
+let lastname = 'Jackson';
+var fullname = firstname + " " + lastname;
+
+console.log("10" + 12);//1012
+let a = '10';
+let b = 10;
+if(a === b){
+    console.log("ok");
+}
+else{
+    console.log("No ok");
+}
+
+console.log("5" * 500000000000);
+
+console.log(5 * 500000000000);
